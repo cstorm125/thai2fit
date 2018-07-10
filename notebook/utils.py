numericalizer bug

notebook/utils.py
@@ -59,20 +59,21 @@ def get_all(df):
     return tok, labels
 
 #convert text dataframe to numericalized dataframes
-def numericalizer(df, max_vocab = 60000, min_freq = 2, pad_tok = '_pad_',
+def numericalizer(df, itos=None, max_vocab = 60000, min_freq = 2, pad_tok = '_pad_',
             unk_tok = '_unk_'):
     tok, labels = get_all(df)
     freq = Counter(p for o in tok for p in o)
-    itos = [o for o,c in freq.most_common(max_vocab) if c>min_freq]
-    itos.insert(0, pad_tok)
-    itos.insert(0, unk_tok)
+    if itos is None:
+        itos = [o for o,c in freq.most_common(max_vocab) if c>min_freq]
+        itos.insert(0, pad_tok)
+        itos.insert(0, unk_tok)
     stoi = collections.defaultdict(lambda:0, {v:k for k,v in enumerate(itos)})
     lm = np.array([[stoi[o] for o in p] for p in tok])
     return(lm,tok,labels,itos,stoi,freq)
 
 #get document vectors from language model
 def document_vector(ss, m, stoi,tok_engine='newmm'):
-    s = word_tokenize(ss,tok_engine)
+    s = word_tokenize(ss)
     t = LongTensor([stoi[i] for i in s]).view(-1,1).cuda()
     t = Variable(t,volatile=False)
     m.reset()